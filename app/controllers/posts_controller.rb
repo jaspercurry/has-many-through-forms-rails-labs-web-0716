bb2fcab50e3deeb78bfac451a6cbd7973e0f0a24
jaspercurry <jaspercurry@gmail.com>
2016-08-23 22:24:41 -0400

    made lab

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,21 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
+    @comment_users = []
+
+    @user_array = []
+    @post.comments.each do |comm|
+      @user_array << comm.user
+    end
+    
+    @final = @user_array.uniq {|obj| obj.id}
+
+
+
+
+
+
   end
 
   def index
@@ -12,13 +27,20 @@ class PostsController < ApplicationController
   end
 
   def create
+
+
     post = Post.create(post_params)
+    # unless params[:post][:category][:name] == ""
+    #   cat = Category.create(name: params[:post][:category][:name])
+    #   post.categories << cat
+    # end
     redirect_to post
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name] )
+
   end
 end
